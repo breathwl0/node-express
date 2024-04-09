c92a29c77799cc02de105f210a0f966e34ef0759
breathwl <breathwl@gmail.com>
2024-04-09 12:20:12 +0000

    feat: init

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')))
   .set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index1');
+  res.render('index2');
 });
 
 app.get('/api', (req, res) => {
